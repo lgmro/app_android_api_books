2e772671e1a5496959b5a700ad873e29dd87741b
lgmro <lucasgmmro@gmail.com>
2022-06-11 18:05:22 -0300

    Refactor and creating packages.

--- app/src/main/java/com/lgmro/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/lgmro/nybooks/presentation/books/BooksActivity.kt
@@ -1,11 +1,12 @@
-package com.lgmro.nybooks
+package com.lgmro.nybooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lgmro.nybooks.R
 
-class MainActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_books)
     }
 }
