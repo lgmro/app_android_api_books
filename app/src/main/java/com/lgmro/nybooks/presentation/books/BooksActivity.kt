74827bd19e84d845d4a7fd1fd58cf63262812391
lgmro <lucasgmmro@gmail.com>
2022-06-11 18:35:35 -0300

    creating actionBar and add recyclerview

--- app/src/main/java/com/lgmro/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/lgmro/nybooks/presentation/books/BooksActivity.kt
@@ -1,12 +1,19 @@
 package com.lgmro.nybooks.presentation.books
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lgmro.nybooks.R
+import androidx.appcompat.app.AppCompatActivity
+import com.lgmro.nybooks.databinding.ActivityBooksBinding
 
 class BooksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBooksBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_books)
+        binding = ActivityBooksBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val toolbarMain = binding.toolbarMain
+        setSupportActionBar(toolbarMain)
     }
 }
