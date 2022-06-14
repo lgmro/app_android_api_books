a0b0984495e8c2f2fbe9528b7eccd44329d84615
lgmro <lucasgmmro@gmail.com>
2022-06-13 21:52:49 -0300

    Create data class response and finish end point

--- app/src/main/java/com/lgmro/nybooks/data/ApiNewYorkTimes.kt
+++ app/src/main/java/com/lgmro/nybooks/data/ApiNewYorkTimes.kt
@@ -1,10 +1,15 @@
 package com.lgmro.nybooks.data
 
+import com.lgmro.nybooks.data.response.BookBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiNewYorkTimes {
 
     @GET("lists.json")
-    fun listRespos(): Call<List<BookResponse>>
+    fun listRespos(
+        @Query("api-key") apiKey: String = "wO6qGhGNtyZPo4CXPQkCyB7SwFSdCk28",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<BookBodyResponse>
 }
